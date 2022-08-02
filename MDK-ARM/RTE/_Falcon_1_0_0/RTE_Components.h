c78e9697fdc6938e5006f4b28f98896817907cb3
OrangerLee <75459917+OrangerLee@users.noreply.github.com>
2022-08-02 19:54:31 +0800

    Add files via upload

---
+++ MDK-ARM/RTE/_Falcon_1_0_0/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Falcon_1_0_0' 
+ * Target:  'Falcon_1_0_0' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
